933c02f48df56de4e3e031d8534670afbbd26a01
taniya jaison <taneahjaison77@gmail.com>
2024-05-25 22:57:39 +0530

    first commit

--- sub.py
+++ sub.py
@@ -1,10 +1,3 @@
-# This program adds two numbers
+# This program prints Hello, world!
 
-num1 = 1.6
-num2 = 9
-
-# Add two numbers
-sum = num1 + number2
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('Hello, world!')
