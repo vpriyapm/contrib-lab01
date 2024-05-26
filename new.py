01c9db6301e6f05d875d2f22210eb6c9ba5313f1
taniya jaison <taneahjaison77@gmail.com>
2024-05-26 11:49:03 +0530

    first commit

--- new.py
+++ new.py
@@ -12,5 +12,5 @@ temp = x
 x = y
 y = temp
 
-print('The value of x after swapping: {}'.format(x))
-print('The value of y after swapping: {}'.format(y))
+print('The value of x after swapping: {}'.format(x));
+print('The value of y after swapping: {}'.format(y));
